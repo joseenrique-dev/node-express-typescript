3e4a49c0c1ab54bd0d3f3757780b40fa49fb8fdc
José Enrique <otipip90@gmail.com>
2022-02-02 02:50:07 -0500

    added CRUD controller, added test http routes and test REST CLIENT VSC plugin. Finish API express test.

--- src/routes/videos.controller.ts
+++ src/routes/videos.controller.ts
@@ -1,24 +1,83 @@
 import { RequestHandler } from 'express';
 import Video from './Video';
 
-export const getVideos: RequestHandler = (req, res) =>
-    res.json('getting videos');
-
-export const getVideo: RequestHandler = (req, res) =>
-    res.json('getting single video...');
-
-export const createVideo: RequestHandler = async (req, res) => {
-    const videoFound = await Video.findOne({ url: req.body.url });
-    if (videoFound)
-        res.status(301).json({ message: 'The url already exist...' });
-    const videoModel = new Video(req.body);
-    console.log(videoModel);
-    const saveVideo = await videoModel.save();
-    res.json(saveVideo);
+/**
+ * Get All Videos
+ * @param req
+ * @param res
+ */
+export const getVideos: RequestHandler = async (req, res) => {
+    try {
+        const allVideos = await Video.find();
+        res.json(allVideos);
+    } catch (error) {
+        res.json(error);
+    }
 };
 
-export const updateVideo: RequestHandler = (req, res) =>
-    res.json('getting videos');
+/**
+ * Get Single Video
+ * @param req
+ * @param res
+ * @returns
+ */
+export const getVideo: RequestHandler = async (req, res) => {
+    try {
+        const foundVideo = await Video.findById(req.params.id);
+        if (!foundVideo) return res.status(204).json();
+        return res.json(foundVideo);
+    } catch (error) {
+        res.json(error);
+    }
+};
 
-export const removeVideo: RequestHandler = (req, res) =>
-    res.json('getting videos');
+/**
+ * Create Video
+ * @param VideoSchema
+ * @returns res
+ */
+export const createVideo: RequestHandler = async (req, res) => {
+    try {
+        const videoFound = await Video.findOne({ url: req.body.url });
+        if (videoFound)
+            return res
+                .status(301)
+                .json({ message: 'The url already exist...' });
+        const videoModel = new Video(req.body);
+        const saveVideo = await videoModel.save();
+        res.json(saveVideo);
+    } catch (error) {
+        res.json(error);
+    }
+};
+
+/**
+ * Update Video
+ * @param req
+ * @param res
+ * @returns
+ */
+export const updateVideo: RequestHandler = async (req, res) => {
+    try {
+        const videoUpdated = await Video.findByIdAndUpdate(
+            req.params.id,
+            req.body
+        );
+        if (!videoUpdated) return res.status(204).json();
+        res.json(videoUpdated);
+    } catch (error) {}
+};
+
+/**
+ * Remove Video
+ * @param req
+ * @param res
+ * @returns
+ */
+export const removeVideo: RequestHandler = async (req, res) => {
+    try {
+        const foundVideo = await Video.findByIdAndDelete(req.params.id);
+        if (!foundVideo) return res.status(204).json();
+        res.json(foundVideo);
+    } catch (error) {}
+};
