7e61095625ea030bf646303896a2c93032a84cda
José Enrique <otipip90@gmail.com>
2022-02-02 02:13:43 -0500

    customizing response given validation in videos controller file.

--- src/routes/videos.controller.ts
+++ src/routes/videos.controller.ts
@@ -2,20 +2,23 @@ import { RequestHandler } from 'express';
 import Video from './Video';
 
 export const getVideos: RequestHandler = (req, res) =>
-  res.json('getting videos');
+    res.json('getting videos');
 
 export const getVideo: RequestHandler = (req, res) =>
-  res.json('getting single video...');
+    res.json('getting single video...');
 
 export const createVideo: RequestHandler = async (req, res) => {
-  const videoModel = new Video(req.body);
-  console.log(videoModel);
-  const saveVideo = await videoModel.save();
-  res.json(saveVideo);
+    const videoFound = await Video.findOne({ url: req.body.url });
+    if (videoFound)
+        res.status(301).json({ message: 'The url already exist...' });
+    const videoModel = new Video(req.body);
+    console.log(videoModel);
+    const saveVideo = await videoModel.save();
+    res.json(saveVideo);
 };
 
 export const updateVideo: RequestHandler = (req, res) =>
-  res.json('getting videos');
+    res.json('getting videos');
 
 export const removeVideo: RequestHandler = (req, res) =>
-  res.json('getting videos');
+    res.json('getting videos');
